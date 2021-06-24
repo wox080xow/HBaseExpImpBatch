8d8711b81c597e14b5cd932bd46aefb6365a9af7
wox080xow <b04302127@g.ntu.edu.tw>
2021-06-24 12:17:47 +0800

    update string processing

--- tmp.stringproc.sh
+++ tmp.stringproc.sh
@@ -1,7 +1,28 @@
+createtable=""
 while read l
 do
-  if [[ $l =~ $ ]]
+  table="Table"
+  #cf="{NAME"
+  cf="NAME =>"
+  #echo $l
+  #echo -e "\n*****createtable string:\n$createtable\n"
+  if [[ $l =~ $table ]]
   then
+    echo $l|cut -d' ' -f2
+    echo 'this is table'
+    createtable="create $(echo $l|cut -d' ' -f2)"
+    echo -e "\n*****createtable string:\n$createtable\n"
   fi
-  
+  if [[ $l =~ $cf ]]
+  then
+    echo $l|sed 's/FOREVER/org.apache.hadoop.hbase.HConstants::FOREVER/'
+    echo 'this is column family'
+    createtable="$createtable, $(echo $l|sed 's/FOREVER/org.apache.hadoop.hbase.HConstants::FOREVER/')"
+    echo -e "\n*****createtable string:\n$createtable\n"
+  fi
+  if [[ $l = "nil" ]]
+  then
+    createtable=""
+    echo 'the end of desc'
+  fi
 done <desc.list.tmp
