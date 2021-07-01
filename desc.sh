9f7cd59f7a1676945ad56a45a4a05b2b1625ef27
wox080xow <b04302127@g.ntu.edu.tw>
2021-07-01 18:09:26 +0800

    desc.sh update

--- desc.sh
+++ desc.sh
@@ -1,14 +1,16 @@
-echo "list"|hbase shell -n >list.hb.tmp
-lines=$(($(($(cat list.hb.tmp|wc -l)-3))/2))
-cat list.hb.tmp|tail -n $lines >table.list
+echo "list"|hbase shell -n >list.list.tmp
+lines=$(($(($(cat list.list.tmp|wc -l)-3))/2))
+cat list.list.tmp|tail -n $lines >table.list.tmp
+rm -rf list.list.tmp
 
 b=""
 while read t
 do 
   a="desc '$t'"
   b=$b"\n"$a
-done <table.list
-rm -rf table.list
+done <table.list.tmp
+rm -rf table.list.tmp
 
-echo -e $b|hbase shell -n >>desc.list
-echo "desc.list is created."
+desclist="OMNI_TMP_FILES/desc.list.tmp"
+echo -e $b|hbase shell -n >>$desclist
+echo "$desclist is created."
