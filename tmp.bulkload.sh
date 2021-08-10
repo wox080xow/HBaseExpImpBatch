eb4bfb6b19f92c092b7cacffe3d29640c2b6bac2
wox080xow <b04302127@g.ntu.edu.tw>
2021-08-10 13:21:49 -0400

    tmp.bulkload.sh add flush table imported and pass test

--- tmp.bulkload.sh
+++ tmp.bulkload.sh
@@ -52,31 +52,59 @@ endtime=$(date -d $2 +%s)000
 tablelist="${tmpdir}table.list-$1-$2.tmp" # tablelist generated from import.sh
 rclistI="${tmpdir}import.rc.table.list-$1-$2.tmp" # rclist generated from import.sh
 
+flushtablelist="${tmpdir}flushtable.list-$1-$2.tmp" # habase shell drop 'table' input
+tableflushedlist="${tmpdir}tableflushed.out.list-$1-$2.tmp" # hbase shell disable 'talbe' output
+
 # COPY TMP TABLE HFILE
 srcdir="/hbase/data/default/" # for CDP
 # srcdir="/apps/hbase/data/data/default/" # for HDP
 destdir="/tmp/bulkload/"
 
+# FLUSH TABLE
+while read t
+do
+  tableAf=$(echo $t|sed "s/^/\'/;s/$/\'/")
+  flushtable="flush $tableAf"
+  echo $flushtable >>$flushtablelist
+done <$tablelist
+
+# flush table through hbase shell
+ftb=""
+while read l
+do
+  fta=$l
+  ftb=$ftb"\n"$fta
+done <$flushtablelist
+
+echo -e $ftb
+echo -e $ftb|hbase shell -n >>$tableflushedlist
+echo "Tables above are flushed."
+
+# COPY HFILE
 banner "COPY HFILE"
 while read t
 do
   tmpt="${t}_OMNI_TMP"
   srchfile="$srcdir$tmpt"
   # srchfile="$srcdir$t"
-  hdfs dfs -cp $srchfile $destdir
+  echo "hdfs dfs -rm -r $destdir$tmpt"
+  hdfs dfs -rm -r $destdir$tmpt
+  echo "hdfs dfs -cp $srchfile $destdir"
+  hdfs dfs -cp $srchfile $destdir 
 done <$tablelist
+
 
 # BULKLOAD TABLE
 banner "START TABLE BULKLOAD"
 
 while read t
 do
-  # tmpt="${t}_OMNI_TMP"
-  hdfsout="${tmpdir}hdfs-$t.out.tmp"
-  bulkloadout="${tmpdir}bulkload-$t.out.tmp"
+  tmpt="${t}_OMNI_TMP"
+  hdfsout="${tmpdir}hdfs-$t-$1-$2.out.tmp"
+  bulkloadout="${tmpdir}bulkload-$t-$1-$2.out.tmp"
   
   # hdfs dfs -du -h /tmp/bulkload/$t | grep -P "[[:xdigit:]]{32}$" | tr [:space:] ',' | sed 's/,,./,/g' | cut -d ',' -f 3 >$hdfsout
-  hdfs dfs -du -h /tmp/bulkload/$t >$hdfsout
+  hdfs dfs -du -h /tmp/bulkload/$tmpt >$hdfsout
   
   phase "table $t"
   # re="(\/[a-zA-Z0-9_-]+)+$"
@@ -87,14 +115,15 @@ do
     if [[ $string =~ $re ]]
     then
       path=${BASH_REMATCH[0]}
-      echo $path
+      # echo $path
       echo "hbase org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles $path $t"
-      # hbase org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles $path $t >$bulkloadout 2>&1
+      hbase org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles $path $t >$bulkloadout 2>&1
     fi
   done <$hdfsout
 
 done <$tablelist
 
+# ROWCOUNT TABLE
 while read t
 do
   # files
